0e8e333f4b034a9f3e7d44d7be901694b4a18a90
HHyeongSeok <gudtjr2454@gmail.com>
2024-03-27 11:16:04 +0900

    [Feat] XML 저장까지

---
+++ UnityProject_24_3_AB/Assets/Scripts/Character/ExCharacterFast.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExCharacterFast : ExCharacter
+{
+    //override ¿Á¡§¿«
+    protected override void Move()
+    {
+        transform.Translate(
+            Vector3.forward * speed * 2 * Time.deltaTime);
+    }
+}
